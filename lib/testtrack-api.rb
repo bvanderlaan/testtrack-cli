1d3540182bb87a0a0ae5503e0895ac8862444863
Brad van der Laan <brad.vanderlaan@christiedigital.com>
2016-06-01 11:37:20 -0400

    Update TestTrack-API to re-throw custom exceptions. This will further hide the interal workings by not allowing third party library exceptions to be thrown. Now the consumer of this API will only need to know about this API's exceptions.

--- lib/testtrack-api.rb
+++ lib/testtrack-api.rb
@@ -1,10 +1,12 @@
 require_relative "testtrack/version"
+require_relative "testtrack/exceptions"
 require 'savon'
 
 module TestTrack
 	class TestTrackApi
 		def initialize(server)
-			@client = Savon.client(wsdl: "http://#{server}/ttsoapcgi.wsdl", 
+			@wsdl_url = "http://#{server}/ttsoapcgi.wsdl"
+			@client = Savon.client(wsdl: @wsdl_url, 
   						headers: { "Authentication" => "secret" },
   						filters: [:password] )
 
@@ -12,7 +14,14 @@ module TestTrack
 		end
 
 		def list_projects(username, password)
-			response = @client.call(:get_project_list, message: { username: "#{username}", password: "#{password}" } )
+			begin
+				response = @client.call(:get_project_list, message: { username: "#{username}", password: "#{password}" } )
+			rescue SocketError
+				raise InvalidURL, "Can not find WSDL with url [#{@wsdl_url}]"
+			rescue Savon::SOAPFault => error
+				raise APIError, error.to_hash[:fault][:faultstring]
+			end
+
   			return response.body			
   		end
 	end
