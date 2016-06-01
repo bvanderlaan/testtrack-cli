8407c6d4e0e2514998eea86cbfaefc091961ccc0
Brad van der Laan <brad.vanderlaan@christiedigital.com>
2016-05-31 21:12:40 -0400

    Add support for sub commands. There is now a 'project' command with its own 'list' sub command. the sub commands implementation is not yet added.

--- lib/testtrack-cli.rb
+++ lib/testtrack-cli.rb
@@ -2,24 +2,10 @@ require 'thor'
 require 'fileutils'
 
 module TestTrack
-	class CLI < Thor
-		include Thor::Actions
-
+	class CLIBase < Thor
 		LOGIN_AUTH_FILE = ".testtrack_auth"
 
-		class_option :force_login, type: :boolean, alias: '-l'
-
-		desc 'login', "Stores your TestTrack username/password so you don't have to enter them each time."
-		def login()
-			auth_from_prompt()
-
-			if yes?("[Auth] Do you want me to save your credentials?")
-				create_file LOGIN_AUTH_FILE, "#{@username} #{@password}"
-			end
-		end
-
-
-	private
+	protected
 		def auth()
 			begin
 				auth_from_file 
@@ -34,7 +20,7 @@ module TestTrack
 			puts "\n\n"
 		end
 
-		def autho_from_file()
+		def auth_from_file()
 			data = open(LOGIN_AUTH_FILE).read if !options[:force_login] && File.exists?(LOGIN_AUTH_FILE)
 			parts = data.split(' ')
 
@@ -42,6 +28,35 @@ module TestTrack
 
 			@username = parts[0]
 			@password = parts[1]
+		end	
+	end
+
+	class Project < CLIBase
+
+		desc 'list', 'List all projects you have access to.'
+		def list()
+			auth()
+
+			puts "This has been a test..."
 		end
+	end
+
+	class CLI < CLIBase
+		include Thor::Actions
+
+		class_option :force_login, type: :boolean, alias: '-l'
+
+		desc 'login', "Stores your TestTrack username/password so you don't have to enter them each time."
+		def login()
+			auth_from_prompt()
+
+			if yes?("[Auth] Do you want me to save your credentials?")
+				create_file LOGIN_AUTH_FILE, "#{@username} #{@password}"
+			end
+		end
+
+		desc "project [SUBCOMMAND] [ARGS]", "tbd"
+		subcommand 'project', Project
+	
 	end
 end
