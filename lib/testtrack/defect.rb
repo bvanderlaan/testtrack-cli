edf391625c12062cc49d502ca8402ca727f72454
Brad van der Laan <brad.vanderlaan@christiedigital.com>
2016-06-02 00:21:16 -0400

    Add more fields to the Defect pretty print. This change set adds 'Fix for', 'Story Points', 'Found in', and 'Found on' fields to the pretty print output of Defect objects. It also adds safty checks to only print fields which the object has. It was found that some Defect types do _not_ have the 'Priority' field.

--- lib/testtrack/defect.rb
+++ lib/testtrack/defect.rb
@@ -20,18 +20,22 @@ module TestTrack
 		def to_s
 			date_time_format = "%a %B %d %Y, %I:%M:%S %p %Z"
 			puts
-			puts "Defect Id:\t #{@data.recordid}"
-			puts "Created:\t #{@data.datetimecreated.strftime(date_time_format)}"
-			puts "Last Modified:\t #{@data.datetimemodified.strftime(date_time_format)}"
-			puts "State:\t\t #{@data.state}"
-			puts "Priority:\t #{@data.priority}"
-			puts "Severity:\t #{@data.severity}"
-			puts "Type:\t\t #{@data.type}"
-			puts "Product:\t #{@data.product}"
-			puts "Component:\t #{@data.component}"
-			puts "Found by:\t #{@data.enteredby}"
+			puts "Defect Id:\t #{@data.recordid}" if @data.respond_to? :recordid
+			puts "Created:\t #{@data.datetimecreated.strftime(date_time_format)}" if @data.respond_to? :datetimecreated
+			puts "Last Modified:\t #{@data.datetimemodified.strftime(date_time_format)}" if @data.respond_to? :datetimemodified
+			puts "State:\t\t #{@data.state}" if @data.respond_to? :state
+			puts "Priority:\t #{@data.priority}" if @data.respond_to? :priority
+			puts "Severity:\t #{@data.severity}" if @data.respond_to? :severity
+			puts "Type:\t\t #{@data.type}" if @data.respond_to? :type
+			puts "Product:\t #{@data.product}" if @data.respond_to? :product
+			puts "Component:\t #{@data.component}" if @data.respond_to? :component
+			puts "Found by:\t #{@data.enteredby}" if @data.respond_to? :enteredby
+			puts "Found in:\t #{@data.custom_field_list.item[0].value}"
+			puts "Found on:\t #{@data.custom_field_list.item[1].value}"
+			puts "Story Points:\t #{@data.custom_field_list.item[5].value}"
+			puts "Fix for:\t #{@data.custom_field_list.item[9].value}"
 			puts
-			puts "\t#{@data.summary}"
+			puts "\t#{@data.summary}" if @data.respond_to? :summary
 			puts
 		end
 	end
